d053feef607d029be143cc83af5c08b012c36eb2
KarmaBait <92960553+KarmaBait@users.noreply.github.com>
2022-01-03 21:37:41 +0100

    Version 1.3
    + Added stronger passwords (including special characters)

--- generator.py
+++ generator.py
@@ -12,6 +12,7 @@ token = NULL
 password = NULL
 mailPrefix = NULL
 mailSuffix = NULL
+logCustomName = NULL
 
 #USE SO THAT PROGRAM WON'T LAG / CRASH
 if os.name in ['nt']:
@@ -21,18 +22,21 @@ if os.name in ['nt']:
 #Reads config file & generates token depending on length given
 config = open("config.json")
 cData = json.load(config)
+
 for i in cData["TokenLength"]:
   if int(i) == 4:
-    token = random.randint(1000, 9999)
+        token = random.randint(1000, 9999)
 
   elif int(i) == 5:
-    token = random.randint(10000, 99999)
+        token = random.randint(10000, 99999)
 
   elif int(i) == 6:
-    token = random.randint(100000, 999999) 
+        token = random.randint(100000, 999999) 
 
   elif int(i) < 4 or int(i) > 6:
     print("[400] Please don't use a token length smaller than 4 or longer than 6 - check config.json")
+
+
 
 
 
@@ -44,7 +48,7 @@ if not os.path.exists("logs"):
 
 
 #Generates 16 char password including symbols
-alphabet = string.ascii_letters + string.digits
+alphabet = string.ascii_letters + string.digits + "!@#$%^&*()_"
 password = ''.join(secrets.choice(alphabet) for i in range(16))
 
 
@@ -66,9 +70,6 @@ for i in cData["EnableLogs"]:
 
   elif int(i) == 0:
     print("[WARNING]: Logfiles are currently disabled, no logs created.")
-
-  elif int(i) != 1 and 0:
-    print("Error [401]: Check config.json") 
 
 
 
